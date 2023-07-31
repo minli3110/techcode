c610e20ba891f2808ab9a81aee43c0cefa51449b
minli3110 <127458261+minli3110@users.noreply.github.com>
2023-07-31 15:00:25 +0800

    Add files via upload

---
+++ includes/w_header.php
@@ -0,0 +1,35 @@
+<?php include_once 'includes/session.php'?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>UON - <?php echo $title ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="css/site.css" />
+</head>
+<body>
+    <div class="container">
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+            <img src="images/uon_logo.png" alt="UON" width="30" height="24">
+            </a>
+            </div>
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="w_foodcats.php">Food</a>
+            <a class="nav-link" href="w_accommodation.php">Accomodation</a>
+            <a class="nav-link" href="login.php">Login</a>
+        </div>
+        </div>
+    </div>
+    </nav>
+
+    <br></br>
