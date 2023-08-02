b28cc7141e4e0c4ff7530bb7de4d5682fb8c0020
minli3110 <127458261+minli3110@users.noreply.github.com>
2023-08-02 20:49:58 +0800

    Delete includes directory

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<br></br>
-<br></br>
-<br></br>
-
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-  </body>
-</html>
