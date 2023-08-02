68ddacc960694c217d47637b515ee852dedc602d
elily10 <133307913+elily10@users.noreply.github.com>
2023-08-02 09:37:23 +0800

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -2,11 +2,6 @@
 <br></br>
 <br></br>
 
-<div class="d-flex justify-content-between">
-    <p class="text-start">University of Newcastle (SG)</p>
-    <p class="text-center">uonenquiry@newcastle.edu.au</p>
-    <p class="text-end"><?php echo 'Copyright ELP 20' . date('y') ?></p>
-</div>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
--- includes/header.php
+++ includes/header.php
@@ -7,17 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>UON - <?php echo $title ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    
     <link rel="stylesheet" href="css/site.css" />
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav mr-auto">
-          <a class="nav-item nav-link active" href="index.php">Food Categories <span class="sr-only"></span></a>
-          <a class="nav-item nav-link active" href="viewacc.php">Accommodation <span class="sr-only"></span></a>
-          <a class="nav-item nav-link" href="logout.php">Logout</a>
-</nav>
+
     <div class="container">
 
