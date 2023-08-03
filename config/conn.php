f47c6f5f48873bf222a8f7934dbbc579ae95e64a
minli3110 <127458261+minli3110@users.noreply.github.com>
2023-08-03 17:20:21 +0800

    Add files via upload

---
+++ config/conn.php
@@ -0,0 +1,24 @@
+<?php
+    $host = 'localhost';
+    $db = 'techcode';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+    try {
+        $pdo = new PDO($dsn, $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch(PDOException $e) {
+        echo "<h1 class='text-danger'>No Database Found</h1>";
+        //throw new PDOException($e -> getMessage());
+    }
+
+    require_once 'crud.php';
+    require_once 'user.php';
+    $crud = new crud($pdo);
+    $user = new user($pdo);
+
+?>
